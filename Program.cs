1cb2090f3c3e7bf97f44ea14f34c0604c62d0778
jeremyshepherd <jeremy.shepherd@sinclair.edu>
2019-12-31 11:51:59 -0500

    Chapter 06 Try-Catch-Finally Error handling

--- Program.cs
+++ Program.cs
@@ -12,43 +12,28 @@ namespace CSHARP_IN_ONE_DAY_CHAPTERS
         static void Main(string[] args)
         {
 
-            ///HelloWorldAgain - A simple program that accepts user input with basic validation to display a custom message to the console
-            string userName = "";
-            int userAge = 0,
-                currentYear = 0;
+            //Try Catch Finally - Error Handling Demonstration.
+            int choice = 0;
 
-            //GET User Name
-            Console.Write("Please enter your name: ");
-            userName = Console.ReadLine();
-
-            //GET User Age
-            Console.Write("Please enter your age: ");
-            userAge = Convert.ToInt32(Console.ReadLine());
-
-            //Respond accordingly based on age answer
-            if (userAge < 0 || userAge > 100)
+            int[] numbers = { 10, 11, 12, 13, 14, 15 };
+            Console.Write("Please enter the index of the array: ");
+            try
             {
-                Console.WriteLine("Invalid Age");
-                Console.WriteLine("Age must be between 0 and 100");
+                choice = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("numbers[{0}] = {1}", choice, numbers[choice]);
             }
-            else if (userAge < 18)
+            catch (IndexOutOfRangeException)
             {
-                Console.WriteLine("Sorry, you are underage");
+                Console.WriteLine("Error: Index should be from 0 to 5.");
             }
-            else if (userAge < 21)
+            catch (FormatException)
             {
-                Console.WriteLine("You need parental consent");
+                Console.WriteLine("Error: You did not enter an integer.");
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("Congratulations!");
-                Console.WriteLine("You may sign up for this event!");
+                Console.WriteLine(e.Message);
             }
-
-            Console.Write("Please enter the current year: ");
-            currentYear = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Hello World! My name is {0} and I am {1} years olds and I was born in {2}.", userName, userAge, currentYear - userAge);
 
             //For Every Exercise
             Console.Read();
