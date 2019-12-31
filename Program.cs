62a713609657049ecddc2bf40f53d0c61a0bca42
jeremyshepherd <jeremy.shepherd@sinclair.edu>
2019-12-31 09:10:11 -0500

    FIRST COMMIT

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSHARP_IN_ONE_DAY_CHAPTERS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
